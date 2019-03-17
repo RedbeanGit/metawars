b0a1c751bcafbc468a4da05f493b91d66346b65b
Julien Dubois <ptijuju22@gmail.com>
2019-03-17 15:53:55 +0100

    Création des fichiers

---
+++ level.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Gabriel Neny; Colin Noiret; Julien Dubois"
+__version__ = "0.1.0"
+
+
+class Level(object):
+	"""
+	Gere l'ensemble des entités du jeu.
+	"""
+
+	def __init__(self):
+		pass
+
+	def update(self, deltatime):
+		pass
