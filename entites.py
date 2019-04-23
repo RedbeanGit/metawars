b06eee36af19ec9fc6fa63ffb055b2584386a67f
Julien Dubois <ptijuju22@gmail.com>
2019-04-23 17:04:23 +0200

    Bug avec git

--- entites.py
+++ entites.py
@@ -70,12 +70,6 @@ class Joueur(Entite):
 	def recule(self):
 		self.vitesse = -constantes.VITESSE_JOUEUR
 
-	def aller_droite(self):
-		pass
-
-	def aller_gauche(self):
-		pass
-
 	def stop(self):
 		self.vitesse = 0
 
