c2caa56eef71b4425b664d289e97e3e70969e9f1
Julien Dubois <ptijuju22@gmail.com>
2019-04-25 23:42:16 +0200

    Ajout des textures des boutons

--- entites.py
+++ entites.py
@@ -84,15 +84,13 @@ class Joueur(Entite):
 		self.niveau.entites.append(tir)
 
 	def bouge(self,temps):
-		
+		# on redéfinit cette méthode pour changer les déplacements du joueur
+		# il ne dépend plus de son angle de rotation
 		self.position[0] += self.velocite[0] * temps
 		self.position[1] -= self.velocite[1] * temps
-		
-
-		pass
 
 	def haut(self):
-		self.velocite[0] = 0 
+		self.velocite[0] = 0
 		self.velocite[1] = constantes.VITESSE_JOUEUR 
 
 	def bas(self):
@@ -105,11 +103,11 @@ class Joueur(Entite):
 
 	def gauche(self):
 		self.velocite[0] = -constantes.VITESSE_JOUEUR 
-		self.velocite[1] = 0 
+		self.velocite[1] = 0
+
 	def stop(self):
 		self.velocite[0] = 0
 		self.velocite[1] = 0
-
 
 
 class Ennemi(Entite):
